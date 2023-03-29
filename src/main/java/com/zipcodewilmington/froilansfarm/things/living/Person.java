847b307ba757789d92e532119e56a97a30872ec7
Jacob C <jacobc@Zipcoders-MacBook-Pro.local>
2023-03-29 15:43:57 -0400

    fixed person

--- src/main/java/com/zipcodewilmington/froilansfarm/things/living/Person.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/things/living/Person.java
@@ -3,7 +3,7 @@ package com.zipcodewilmington.froilansfarm.things.living;
 import com.zipcodewilmington.froilansfarm.Rideable;
 import com.zipcodewilmington.froilansfarm.Rider;
 
-public abstract class Person extends Animal implements Rider<Rideable> {
+public abstract class Person extends Animal{
     String name;
     public String getName(){
         return this.name;
--- src/test/java/com/zipcodewilmington/froilansfarm/testThings/testLiving/TestAnimal.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/testThings/testLiving/TestAnimal.java
@@ -1,15 +1,5 @@
 package com.zipcodewilmington.froilansfarm.testThings.testLiving;
 
-import com.zipcodewilmington.froilansfarm.things.living.Botanist;
-import com.zipcodewilmington.froilansfarm.things.living.Person;
 
 public class TestAnimal {
- /*   Person person;
-
-    @Before
-    public void Init(){
-        person = new Botanist();
-    }
-    @Test
-    public void test */
 }
