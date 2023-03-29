9e35d11274d09d5b9829e8ec0f2a1a8374a89882
dayvidtran <dayvidtran@gmail.com>
2023-03-29 14:46:32 -0400

    vehicle class

--- src/main/java/com/zipcodewilmington/froilansfarm/things/machine/AirCraft.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/things/machine/AirCraft.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington.froilansfarm.things.machine;
 
 import com.zipcodewilmington.froilansfarm.Rideable;
 import com.zipcodewilmington.froilansfarm.Rider;
+import com.zipcodewilmington.froilansfarm.things.living.Pilot;
 
-public class AirCraft implements Rideable {
+public class AirCraft<Pilot> extends Vehicle {
 }
