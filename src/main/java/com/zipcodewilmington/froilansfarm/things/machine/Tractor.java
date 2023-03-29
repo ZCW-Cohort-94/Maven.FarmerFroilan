2a16ac68e3d92540f2195a4dfa1061d6149b0ae5
dayvidtran <dayvidtran@gmail.com>
2023-03-29 15:19:21 -0400

    tractor implemengintg farmvehicle. cropduster extending to aircraft implementing farmvehicle

--- src/main/java/com/zipcodewilmington/froilansfarm/things/machine/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/things/machine/Tractor.java
@@ -1,9 +1,15 @@
 package com.zipcodewilmington.froilansfarm.things.machine;
 
+import com.zipcodewilmington.froilansfarm.FarmVehicle;
 import com.zipcodewilmington.froilansfarm.storage.CropRow;
 
-public class Tractor extends Vehicle  {
+public class Tractor extends Vehicle implements FarmVehicle {
 
     public void harvest() {
+    }
+
+    @Override
+    public boolean farmOperation() {
+        return false;
     }
 }
