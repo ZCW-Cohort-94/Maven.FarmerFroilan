eb4a6e302e55dc1a3b06a8afee0830eb278353e8
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-03-29 15:07:03 -0400

    Chicken changes

--- src/main/java/com/zipcodewilmington/froilansfarm/things/living/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/things/living/Chicken.java
@@ -1,10 +1,4 @@
 package com.zipcodewilmington.froilansfarm.things.living;
 
-import com.zipcodewilmington.froilansfarm.Producer;
-
-public class Chicken extends Animal implements Producer<Egg> {
-    @Override
-    public void yield() {
-
-    }
+public class Chicken {
 }
