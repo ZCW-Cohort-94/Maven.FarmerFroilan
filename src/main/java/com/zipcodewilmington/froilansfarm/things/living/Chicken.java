2b08cb84f086cc7877eb67cb52b1f9b5752eeb1a
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-03-30 13:14:44 -0400

    chicken changes2

--- src/main/java/com/zipcodewilmington/froilansfarm/things/living/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/things/living/Chicken.java
@@ -1,4 +1,17 @@
 package com.zipcodewilmington.froilansfarm.things.living;
 
-public class Chicken {
+import com.zipcodewilmington.froilansfarm.Edible;
+import com.zipcodewilmington.froilansfarm.Producer;
+
+public class Chicken extends Animal implements Producer{
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void yield(Edible edible) {
+
+    }
 }
