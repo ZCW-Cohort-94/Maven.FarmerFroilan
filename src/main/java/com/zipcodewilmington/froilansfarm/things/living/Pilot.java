c0d5d69d0d077f62bb041ecda9348663ec5af60c
dayvidtran <dayvidtran@gmail.com>
2023-03-30 20:16:51 -0400

    pilot generic aircraft

--- src/main/java/com/zipcodewilmington/froilansfarm/things/living/Pilot.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/things/living/Pilot.java
@@ -3,14 +3,20 @@ package com.zipcodewilmington.froilansfarm.things.living;
 import com.zipcodewilmington.froilansfarm.Rider;
 import com.zipcodewilmington.froilansfarm.things.machine.AirCraft;
 
-public class Pilot extends Person implements Rider<AirCraft> {
-    @Override
-    public boolean mount() {
+public abstract class Pilot extends Person implements Rider<AirCraft> {
+    AirCraft airCraft;
+
+    public boolean mount(AirCraft airCraft) {
+        return false;
+    }
+
+
+    public boolean dismount(AirCraft airCraft) {
         return false;
     }
 
     @Override
-    public boolean dismount() {
-        return false;
+    public String makeNoise() {
+        return null;
     }
 }
