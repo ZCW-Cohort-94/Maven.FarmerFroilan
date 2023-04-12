050d741ce9a0cbd83d0a92a91f2ce3f3e0a4b3d7
nora <sxj042@gmail.com>
2023-04-12 11:38:50 -0400

    added tests 100% machine

--- src/main/java/com/zipcodewilmington/froilansfarm/things/machine/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/things/machine/CropDuster.java
@@ -1,8 +1,9 @@
 package com.zipcodewilmington.froilansfarm.things.machine;
 
 import com.zipcodewilmington.froilansfarm.FarmVehicle;
+import com.zipcodewilmington.froilansfarm.Rider;
 
-public class CropDuster extends AirCraft implements FarmVehicle {
+public class CropDuster<T extends Rider> extends AirCraft implements FarmVehicle<Rider> {
     public void fertilize(){
 
     }
@@ -10,5 +11,10 @@ public class CropDuster extends AirCraft implements FarmVehicle {
     @Override
     public boolean farmOperation() {
         return false;
+    }
+
+    @Override
+    public String makeNoise() {
+        return "CropDuster noise";
     }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/testThings/testMachine/TestCropDuster.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/testThings/testMachine/TestCropDuster.java
@@ -1,4 +1,67 @@
 package com.zipcodewilmington.froilansfarm.testThings.testMachine;
 
+import com.zipcodewilmington.froilansfarm.FarmVehicle;
+import com.zipcodewilmington.froilansfarm.things.living.Farmer;
+import com.zipcodewilmington.froilansfarm.things.machine.AirCraft;
+import com.zipcodewilmington.froilansfarm.things.machine.CropDuster;
+import com.zipcodewilmington.froilansfarm.things.machine.Tractor;
+import com.zipcodewilmington.froilansfarm.things.machine.Vehicle;
+import com.zipcodewilmington.froilansfarm.things.plant.Crops;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class TestCropDuster {
+
+    @Test
+    public void makeNoiseTest(){
+        CropDuster<Farmer> tester = new CropDuster<>();
+
+        String expected = "CropDuster noise";
+        String actual = tester.makeNoise();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testInheritanceCropDuster() {
+        CropDuster<Farmer> tester = new CropDuster<>();
+        Assert.assertTrue(tester instanceof AirCraft);
+    }
+
+    @Test
+    public void testInheritanceCropDuster2() {
+        CropDuster<Farmer> tester = new CropDuster<>();
+        Assert.assertTrue(tester instanceof FarmVehicle);
+    }
+
+
+    @Test
+    public void testGetMounted() {
+        CropDuster<Farmer> tester = new CropDuster<>();
+
+        boolean actual = false;
+        tester.setMounted(actual);
+        boolean expected = tester.getMounted();
+
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testFertilize() {
+        CropDuster<Farmer> tester = new CropDuster<>();
+        tester.fertilize();
+        ArrayList<Crops> testing = new ArrayList<>();
+        Assert.assertNotNull(testing);
+    }
+
+    @Test
+    public void testFarmOperation() {
+        CropDuster<Farmer> tester = new CropDuster<>();
+        boolean actual = tester.farmOperation();
+        Assert.assertFalse(actual);
+    }
+
 }
